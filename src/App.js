8334e9ff69e3f2f9c3f2466f889724b3e0740cb1
vaghelahemdip <vhemdip@gmail.com>
2023-06-21 17:02:41 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ScreenShareComponent from './ScreenShareComponent.tsx';
+import { Router, Route, Routes } from 'react-router';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Screen Sharing Demo</h1>
-      <ScreenShareComponent />
-    </div>
+    // <div className="App">
+    //   <h1>Screen Sharing Demo</h1>
+    //   <ScreenShareComponent />
+    // </div>
+    <Router>
+    <Routes>
+        <Route exact path="/" element={<ScreenShareComponent />}></Route> 
+    </Routes>
+ {/* <Footer /> */}
+  </Router>
   );
 }
 
